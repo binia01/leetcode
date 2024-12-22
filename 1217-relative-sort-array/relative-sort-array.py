d9eb7742c3569a61cd2f4df67944905a87246d31
Binyam Abrha Abay <176568730+binia01@users.noreply.github.com>
2024-12-22 17:37:28 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (5.53%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        ans = []
+        count = [0]*1001
+
+        for arr in arr1:
+            count[arr]+=1
+        for a in arr2:
+            while count[a] > 0:
+                ans.append(a)
+                count[a]-=1
+        for num in range(1001):
+            for _ in range(count[num]):
+                ans.append(num)
+        return ans
+        
