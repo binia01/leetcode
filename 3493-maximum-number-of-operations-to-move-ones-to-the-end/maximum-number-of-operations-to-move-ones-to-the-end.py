1316f24e6db5ae62671d19d51467172bbc97616b
Binyam Abrha Abay <176568730+binia01@users.noreply.github.com>
2024-12-22 12:00:44 +0300

    Time: 79 ms (25.41%) | Memory: 18.3 MB (12.97%) - LeetSync

---
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxOperations(self, s: str) -> int:
+        ans = 0
+        ones = 0
+
+        for i,c in enumerate(s):
+            if s[i] == '1':
+                ones+=1
+            elif i+1 == len(s) or s[i+1] =='1':
+                ans+=ones
+        return ans
