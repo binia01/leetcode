ee9e0e37a38a174cd55f6d13efb222e320bc5c66
Binyam Abrha Abay <176568730+binia01@users.noreply.github.com>
2024-12-22 13:10:10 +0300

    Time: 12 ms (25.79%) | Memory: 17.8 MB (6.75%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        n = len(s)
+        k = len(part)
+
+        t =[" "]*n
+        j = 0
+        for c in s:
+            t[j] = c
+            j+=1
+            if j >=k and ''.join(t[j-k:j]) == part:
+                j-=k
+        return ''.join(t[:j])
+        
