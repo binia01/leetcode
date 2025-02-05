086ab8257acea2f341e7d7c70305152c73dedc40
Binyam Abrha Abay <176568730+binia01@users.noreply.github.com>
2025-02-05 16:26:44 +0300

    Time: 10 ms (8.90%) | Memory: 18.1 MB (19.69%) - LeetSync

--- 1651-shuffle-string/shuffle-string.py
+++ 1651-shuffle-string/shuffle-string.py
@@ -1,20 +1,15 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
-        ans = [""] * len(indices)
-        for i in range(len(indices)):
-            ans[indices[i]] = s[i] 
-        return "".join(ans)
-
-        # b = list(s)
-        # count = []
-        # for i in range(len(b)):
-        #     count.append([b[i], indices[i]])
-        # print(count)
-        # count.sort(key = lambda x: x[1])
-        # ans = ""
-        # for word in count:
-        #     ans += word[0]
-        # return ans
+        b = list(s)
+        count = []
+        for i in range(len(b)):
+            count.append([b[i], indices[i]])
+        print(count)
+        count.sort(key = lambda x: x[1])
+        ans = ""
+        for word in count:
+            ans += word[0]
+        return ans
         
 
         
