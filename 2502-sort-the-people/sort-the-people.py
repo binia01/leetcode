13db00f45f29e0678cb1ee13f4ca1b714c73985b
Binyam Abrha Abay <176568730+binia01@users.noreply.github.com>
2025-02-10 15:56:34 +0300

    Time: 295 ms (9.17%) | Memory: 18 MB (88.03%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -7,13 +7,26 @@ class Solution:
         #             names[j], names[j+1] = names[j+1], names[j]
         # return names
 
-        for i in range(len(heights)):
-            min_num = i
-            for j in range(i+1, len(heights)):
-                if heights[min_num] <  heights[j]:
-                    min_num = j
-            heights[i], heights[min_num] = heights[min_num], heights[i]
-            names[i], names[min_num] = names[min_num], names[i]
+        # for i in range(len(heights)):
+        #     min_num = i
+        #     for j in range(i+1, len(heights)):
+        #         if heights[min_num] <  heights[j]:
+        #             min_num = j
+        #     heights[i], heights[min_num] = heights[min_num], heights[i]
+        #     names[i], names[min_num] = names[min_num], names[i]
+
+        # return names
+
+        for i in range(1, len(heights)):
+            key = heights[i]
+            key_name = names[i]
+            j = i-1
+            while j >=0 and key > heights[j]:
+                heights[j+1] = heights[j]
+                names[j+1] = names[j]
+                j -= 1
+            heights[j+1] = key
+            names[j+1] = key_name
         return names
 
-        
+
